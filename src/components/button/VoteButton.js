55fca2c82367842b780fca64868eb6fb07f584ca
RicardoJ <rjdiazzh@gmail.com>
2020-09-27 13:05:51 -0500

    change name VoteNowButton component to VoteButton component

--- src/components/button/VoteButton.js
+++ src/components/button/VoteButton.js
@@ -1,17 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const VoteNowBtn = styled.button`
+const VoteBtn = styled.button`
   background-color: transparent;
   border: 1px solid #e7e7e7;
   cursor: pointer;
   height: 40px;
-  width: 80px;
+  width: 100px;
   color: white;
   font-size: 14px;
+ 
 `;
-const VoteNowButton = () => {
-  return <VoteNowBtn>Vote Now</VoteNowBtn>;
+const VoteButton = () => {
+  return <VoteBtn>Vote Now</VoteBtn>;
 };
 
-export default VoteNowButton;
+export default VoteButton;
