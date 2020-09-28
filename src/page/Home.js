36326cf7b7fb9fad5529858386b1490498cbb0bf
RicardoJ <rjdiazzh@gmail.com>
2020-09-28 16:55:32 -0500

    adding routes and pages

---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Votes from "./Votes";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Votes />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/page/PastTrial.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PastTrial = () => {
+    return ( <h1>Past Trial</h1> );
+}
+ 
+export default PastTrial;
